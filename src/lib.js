e2000eba0e00263f4ed0de744e82a55311b5b8cf
Shubham Chauhan <cshubham@In-cshubham.local>
2018-11-27 15:35:39 +0530

    Introduce createInitialBoard().
    Introduced function createAliveCells() for createInitialBoard().

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,19 @@
 const boardGenerator = function(size) {
   let board = new Array(size).fill([]);
-  return board.map(x=>new Array(size).fill("dead"));
+  return board.map(element => new Array(size).fill("dead"));
 }
 
+const createAliveCells = function(cells) {
+  return function(position) {
+    cells[position[0]][position[1]] = "alive";
+    return cells;
+  }
+}
+
+const createInitialBoard = function(size,aliveCells) {
+  let emptyBoard = boardGenerator(size);
+  aliveCells.map(createAliveCells(emptyBoard));
+  return emptyBoard;
+}
 exports.boardGenerator = boardGenerator;
+exports.createInitialBoard = createInitialBoard;
--- test/libTest.js
+++ test/libTest.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
-const { boardGenerator } = require('../src/lib.js');
+const { 
+  boardGenerator,
+  createInitialBoard } = require('../src/lib.js');
 
 describe("boardGenerator", function() {
   it("should return a Array of Arrays of same given length", function() {
@@ -10,5 +12,17 @@ describe("boardGenerator", function() {
   it("should return a Array of Arrays of same given length", function() {
     let result = [ [ 'dead', 'dead', 'dead' ],[ 'dead', 'dead', 'dead' ],[ 'dead', 'dead', 'dead' ] ]
     assert.deepEqual(boardGenerator(3), result);
+  });
+});
+
+describe("createInitialBoard", function() {
+  it("should replace given position elements of board  with 'alive'", function() {
+    let result = [ [ 'alive', 'dead' ], [ 'dead', 'alive' ] ];
+    assert.deepEqual(createInitialBoard(2,[[0,0],[1,1]]), result);
+  });
+
+  it("should replace given position elements of board  with 'alive'", function() {
+    let result = [ [ 'alive', 'dead', 'alive' ],[ 'dead', 'alive', 'dead' ],[ 'dead', 'dead', 'alive' ] ]
+    assert.deepEqual(createInitialBoard(3,[[0,0],[1,1],[2,2],[0,2]]), result);
   });
 });
